2c09b45cbedc472306e716b68637035834f56633
Grizzly <gabilomtatidze@gmail.com>
2026-03-03 20:04:51 +0400

    add inputs

--- text/src/pages/TextComparison.tsx
+++ text/src/pages/TextComparison.tsx
@@ -1,11 +1,16 @@
 import down_icon from "../assets/down_icon.png"
 import plus_icon from "../assets/plus_icon.png"
 import { useState } from "react";
+import nextTo_icon from "../assets/nextTo_icon.png"
 
 export default function TextComparison() {
 
     const [open, setOpen] = useState<boolean>(false);
     const [selected, setSelected] = useState<string>("ქართული");
+    const [inputOne, setInputOne] = useState<string>("");
+    const [inputTwo, setInputTwo] = useState<string>("");
+
+    const bothEmpty = !inputOne || !inputTwo;
 
     const options = ["ქართული", "English"];
 
@@ -16,33 +21,33 @@ export default function TextComparison() {
                     <div className="top_area">
                         <div className="left_side">
                             <div className="select" onClick={() => setOpen(!open)}>
-        <p className="select_text">{selected}</p>
-        <img
-          src={down_icon}
-          alt="down_icon"
-          className={open ? "rotate" : ""}
-        />
-      </div>
+                                <p className="select_text">{selected}</p>
+                                <img
+                                    src={down_icon}
+                                    alt="down_icon"
+                                    className={open ? "rotate" : ""}
+                                />
+                            </div>
 
-      {open && (
-        <div className="dropdown">
-          {options.map((item) => (
-            <label key={item} className="option">
-              <input
-                type="radio"
-                name="language"
-                value={item}
-                checked={selected === item}
-                onChange={() => {
-                  setSelected(item);
-                  setOpen(false);
-                }}
-              />
-              <span className="radio_text">{item}</span>
-            </label>
-          ))}
-        </div>
-      )}
+                            {open && (
+                                <div className="dropdown">
+                                    {options.map((item) => (
+                                        <label key={item} className="option">
+                                            <input
+                                                type="radio"
+                                                name="language"
+                                                value={item}
+                                                checked={selected === item}
+                                                onChange={() => {
+                                                    setSelected(item);
+                                                    setOpen(false);
+                                                }}
+                                            />
+                                            <span className="radio_text">{item}</span>
+                                        </label>
+                                    ))}
+                                </div>
+                            )}
                             <input type="checkbox" className="check_box" />
                             <p className="check_box_text">ფორმატის შენარჩუნება</p>
                         </div>
@@ -51,6 +56,16 @@ export default function TextComparison() {
                             ახალის გახსნა
                         </button>
                     </div>
+
+                    <div className="logic_area">
+                        <div className="input_area">
+                            <textarea id="" placeholder="დაიწყე დაწერა..." value={inputOne} onChange={(e) => setInputOne(e.target.value)} />
+                            <img src={nextTo_icon} alt="nextTo_icon" style={{width: "32px", height: "32px"}}/>
+                            <textarea id="" placeholder="დაიწყე დაწერა..." value={inputTwo} onChange={(e) => setInputTwo(e.target.value)} />
+                        </div>
+                        <button style={{ backgroundColor: bothEmpty ? "#888991" : "#4571E4" }}>შედარება</button>
+                    </div>
+
                 </div>
             </div>
         </>
